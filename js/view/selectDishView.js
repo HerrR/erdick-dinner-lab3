b5328293ef18fe7d835df8c526159af010c48368
HerrR <ribe93@live.se>
2015-02-10 16:12:25 +0100

    More interaction working

--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -13,28 +13,30 @@ var SelectDishView = function(container, model) {
 		var numberOfDishes = allSelected.length;
 
 		returnstring += "<div class='row offset' id='topDishRow'>";
-		for(i = 0;i<numberOfDishes; i++){
-			returnstring += "<div class='col-md-2'>";
-			returnstring += "<img src='images/"+allSelected[i].image+"' class='img-thumbnail'>";
+		for(var i = 0;i<numberOfDishes; i++){
+			returnstring += "<div class='col-md-3'>";
+			returnstring += "<img src='images/"+allSelected[i].image+"' class='img-thumbnail' id='dishImage'>";
+			returnstring += "<div style='font-weight:bold'>"+allSelected[i].name+"</div>";
+			returnstring += allSelected[i].description;
 			returnstring += "</div>";
 		}
 		returnstring += "</div>";
 
-		returnstring += "<div class='row offset'>";
-		for(j = 0;j<numberOfDishes; j++){
-			returnstring += "<div class='col-md-2' style='font-weight:bold;'>";
-			returnstring += allSelected[j].name;
-			returnstring += "</div>";
-		}
-		returnstring += "</div>";
+		// returnstring += "<div class='row offset'>";
+		// for(var j = 0;j<numberOfDishes; j++){
+		// 	returnstring += "<div class='col-md-2' style='font-weight:bold;'>";
+		// 	returnstring += allSelected[j].name;
+		// 	returnstring += "</div>";
+		// }
+		// returnstring += "</div>";
 
-		returnstring += "<div class='row offset'>";
-		for(k = 0;k<numberOfDishes; k++){
-			returnstring += "<div class='col-md-2' style='font-size:8pt;'>";
-			returnstring += allSelected[k].description;
-			returnstring += "</div>";
-		}
-		returnstring += "</div>";
+		// returnstring += "<div class='row offset'>";
+		// for(var k = 0;k<numberOfDishes; k++){
+		// 	returnstring += "<div class='col-md-2' style='font-size:8pt;'>";
+		// 	returnstring += allSelected[k].description;
+		// 	returnstring += "</div>";
+		// }
+		// returnstring += "</div>";
 
 		return returnstring;
 	}
