f7c8bf773ff10e6a98ceaebef9661e8f4e0f1f24
ankhuve <erik.c.forsberg@gmail.com>
2015-02-10 15:43:08 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	index.html
    
    Added navigation between views

---
+++ js/displayViews.js
@@ -0,0 +1,39 @@
+$("#backToEdit").click(function(){
+    $("#dinnerOverviewView").hide();
+});
+
+$("#toNewDinner").click(function(){
+    $("#divOfStartscreenness").hide();
+    $("#exampleView").show();
+    $(".mainHeader").show();
+    $("#selectDish").show();
+});
+
+$("#confirmDinner").click(function(){
+    $("#exampleView").hide();
+    $("#selectDish").hide();
+    $("#dinnerOverviewView").show();
+
+});
+
+$(".backToEdit").click(function(){
+	$("#dinnerOverviewView").hide();
+    $("#dinnerPreparationView").hide();
+    $("#exampleView").show();
+    $("#selectDish").show();
+});
+
+$("#printRecipe").click(function(){
+    $("#dinnerOverviewView").hide();
+    $("#dinnerPreparationView").show();
+
+});
+
+$("h1").click(function(){
+	$("#dinnerOverviewView").hide();
+    $("#dinnerPreparationView").hide();
+    $("#exampleView").hide();
+    $("#selectDish").hide();
+    $(".mainHeader").hide();
+    $("#divOfStartscreenness").show();
+});
